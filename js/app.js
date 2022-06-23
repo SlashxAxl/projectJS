8d3f0d6040c3450c0493af577a85534f58b2cf84
danielmtz <martinezdaniel554@gmail.com>
2022-06-23 13:27:13 -0500

    agregando elementos

--- js/app.js
+++ js/app.js
@@ -1,65 +1,38 @@
-/*let nombre = prompt('Ingrese su nombre')
-let apellido = prompt('Ingrese su apellido')
-
-alert('Bienvenido ' + nombre + ' ' + apellido )
-alert('Esta es una tienda de consola de videojuegos')
-
-const listaProductos = ['PolyStation 5', ' Mexbox Series X ', ' Nientendo Switch HD', ' Juegosfera 3', ' Pici masterreis 2', ' SEGAton', ' Atariado',' Olculus drift 2', ' Pissdeck']
-
-alert('Estos son nuestros productos: ' + listaProductos)
-
-let nombreConsola = prompt('Ingrese el nombre de la consola')
-let precioConsola = Number(prompt('Ingrese el precio de la consola'))
-
-function pedido(){
-    console.log('Consola: ' + nombreConsola + ', ' + 'Precio: ' + precioConsola )
-}
-pedido(nombreConsola, precioConsola)
-
-while (nombreConsola == 'ESC') {
-    pedido()
-}
-
-let pagoTotal = 0
-function totalPagar(precioConsola){
-    pagoTotal = precioConsola
-}
-
-alert('El total de tu pedido es de: ' + totalPagar(pagoTotal))
-
-let direccion_envio = prompt('Ingrese la direccion a la que se enviara su pedido')
-
-alert('Gracias ' + nombre + ' ' + apellido + ' por confiar en nosotros, te enviaremos tu pedido lo mas pronto posible')
-alert('Vuelve pronto!')*/
-
-class Consolas {
-    constructor(marca, modelo,imgSrc ,precio){
-        this.marca = marca
-        this.modelo = modelo
-        this.imgSrc = imgSrc
-        this.precio = precio
-    }
-}
-
-const consola1 = new Consolas ('Sony','Playstation 5','https://m.media-amazon.com/images/I/61gimpyy0UL._AC_SX466_.jpg',13999)
-const consola2 = new Consolas ('Microsoft','Xbox Series X','https://m.media-amazon.com/images/I/61eYoSqkHnL._AC_SX466_.jpg', 13999)
-const consola3 = new Consolas ('Nintendo', 'Switch Oled','https://m.media-amazon.com/images/I/61dYrzvBLbL._AC_SL1483_.jpg', 9999)
-
-const productos = [consola1,consola2,consola3]
+const productos = [producto1, producto2, producto3, producto4, producto5, producto6]
 const cardContainer = document.querySelector('#cardContainer')
-productos.forEach((producto) => {
-    const card = document.createElement('div')
-    card.className = 'card'
-    card.innerHTML = `
-            <h3 class="cardTitle"> Consola ${producto.marca} </h3>
-            <img src="${producto.imgSrc}" class="cardImg">
-            <p class="cardDesc"> ${producto.modelo}</p>
-            <span class="cardPrice"> $${producto.precio} </span>
-            <button data-id="${producto.marca}" class="buttonCTA"> Agregar al Carrito </button>
-        `
-    cardContainer.append(card)
-})
-const botonAgregarProductos = document.querySelector('#agregarProductos')
+
+
+
+const botonRegistro = document.querySelector('#formulario')
+
+const mostrarValue = (e) => {
+    e.preventDefault()
+}
+
+botonRegistro.addEventListener('click', mostrarValue)
+const nombreUsuario = document.getElementById('#name')
+const emailUsuario = document.getElementById('#email')
+if (nombreUsuario === "") {
+
+    alert('¡Porfavor ingresa tus datos para continuar!')
+    
+} else {
+        productos.forEach((producto) => {
+            const card = document.createElement('div')
+            card.className = 'card'
+            card.innerHTML = `
+                    <h3 class="cardTitle"> ${producto.marca} </h3>
+                    <img src="${producto.imgSrc}" class="cardImg">
+                    <p class="cardDesc"> ${producto.modelo}</p>
+                    <span class="cardPrice"> $${producto.precio} </span>
+                    <button data-id="${producto.marca}" class="buttonCTA"> Agregar al Carrito </button>
+                `
+            cardContainer.append(card)
+        })
+}
+
+
+
 let carrito = []
 const agregarProducto = (e) => {
     const productoElegido = e.target.getAttribute('data-id')
@@ -72,10 +45,21 @@ console.log(botonesCompra)
 botonesCompra.forEach((botonCompra) => {
     botonCompra.addEventListener('click', agregarProducto)
 })
-localStorage.setItem('key','value')
 
-const objectLocalstorage = JSON.stringify(Consolas)
-console.log(Consolas)
 
-localStorage.setItem('Objeto', Consolas)
-console.log(document.getElementsByClassName('card'))
+document.querySelector('#finalizarCompra').addEventListener('click',()=>{
+    Toastify({
+        text: "Compra Finalizada!",
+        duration: 3000,
+        destination: "",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "left",
+        stopOnFocus: true,
+        style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+    }).showToast();
+})
---
+++ js/products.js
@@ -0,0 +1,21 @@
+class Productos {
+    constructor(marca, modelo,imgSrc ,precio){
+        this.marca = marca
+        this.modelo = modelo
+        this.imgSrc = imgSrc
+        this.precio = precio
+    }
+}
+
+const producto1 = new Productos ('Sony','Playstation 5','https://m.media-amazon.com/images/I/61gimpyy0UL._AC_SX466_.jpg',13999)
+
+const producto2 = new Productos ('Microsoft','Xbox Series X','https://m.media-amazon.com/images/I/61eYoSqkHnL._AC_SX466_.jpg', 13999)
+
+const producto3 = new Productos ('Nintendo', 'Switch Oled','https://m.media-amazon.com/images/I/61dYrzvBLbL._AC_SL1483_.jpg', 9999)
+
+const producto4 = new Productos ('SEGA', 'SEGA SATURN','https://upload.wikimedia.org/wikipedia/commons/2/20/Sega-Saturn-Console-Set-Mk1.png', 1000)
+
+const producto5 = new Productos ('ATARI', 'ATARI', 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Atari-2600-Wood-4Sw-Set.jpg', 1500)
+
+const producto6 = new Productos ('Gibson', 'Les Paul', 'https://www.falymusic.com/images/detailed/20/LPSS00APNH1_front_copy_opt.jpg', 9000)
+
